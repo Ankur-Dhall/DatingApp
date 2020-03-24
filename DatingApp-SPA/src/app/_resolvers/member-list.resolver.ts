d44f7d0471e7e37c4e695923d6534e46c7d58277
Ankur-Dhall <42307111+Ankur-Dhall@users.noreply.github.com>
2020-03-24 19:08:54 +0530

    Eight Commit : Added Better Looking UI

---
+++ DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Resolve, Router } from '@angular/router';
+import { User } from '../_models/user';
+import { AlertifyService } from '../_services/alertify.service';
+import { UserService } from '../_services/user.service';
+import { ActivatedRouteSnapshot} from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class MemberListResolver implements Resolve<User[]> {
+
+    constructor(private alertify: AlertifyService,
+                private userService: UserService,
+                private router: Router) {}
+
+    resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
+        return this.userService.getUsers().pipe(
+            catchError(error => {
+                this.alertify.error(error);
+                this.router.navigate(['/home']);
+                return of(null);
+            })
+        );
+    }
+}
